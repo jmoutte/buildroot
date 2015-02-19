65a5e7d690ce66820a86e88f912a6d60c28a1098
Julien Moutte <julien@fluendo.com>
2015-02-19 13:56:03 +0100

    Bump Fluendo SS demux to 0.10.30 and remove obsolete patch

--- package/fluendo/flussdemux/flussdemux.mk
+++ package/fluendo/flussdemux/flussdemux.mk
@@ -3,7 +3,7 @@
 # flussdemux
 #
 #############################################################
-FLUSSDEMUX_VERSION = 0.10.27
+FLUSSDEMUX_VERSION = 0.10.30
 FLUSSDEMUX_SOURCE = gst-fluendo-ssdemux-$(FLUSSDEMUX_VERSION).tar.gz
 FLUSSDEMUX_SITE = ~
 FLUSSDEMUX_SITE_METHOD = file
